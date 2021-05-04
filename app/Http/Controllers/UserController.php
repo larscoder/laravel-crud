cee527294d4409b15ceb6d9d18d2b4484359881d
Luis Alberto Ruiz Sarmiento <lars55360@gmail.com>
2021-05-04 07:09:19 -0500

    Agregamos la funcionalidad para eliminar usuarios.

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $user = User::latest()->get();
+        return view('users.index', [
+            'users' => $user
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        User::create([
+            'name' => $request['name'],
+            'email' => $request['email'],
+            'password' => $request['password'],
+        ]);
+
+        return back();
+    }
+
+    public function destroy(User $user)
+    {
+        $user->delete();
+        return back();
+    }
+}
---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <title>Laravel CRUD</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-dm-8 mx-auto">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Nombre</th>
+                            <th>Email</th>
+                            <th>&nbsp;</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($users as $user)
+                            <tr>
+                                <td>{{ $user->id }}</td>
+                                <td>{{ $user->name }}</td>
+                                <td>{{ $user->email }}</td>
+                                <td>
+                                    <form action="{{ route('users.destroy', $user) }}" method="post">
+                                        @method('DELETE')
+                                        @csrf
+                                        <input
+                                            type="submit"
+                                            value="Eliminar"
+                                            class="btn btn-sm btn-danger"
+                                            onclick="return confirm('¿Desea eliminar...?')"
+                                        >
+                                    </form>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
